1f17423b40808d77ec75dea29e72eaf6da2165f0
Suho Park <suho0232@gmail.com>
2022-11-03 18:03:15 +0900

    update : timer 설정 테스트

--- lib/main_reative.dart
+++ lib/main_reative.dart
@@ -4,6 +4,7 @@ import 'dart:math';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const ReactiveDemo());
@@ -44,6 +45,10 @@ class _DemoPageState extends State<DemoPage> {
     startScan();
     Future.delayed(const Duration(seconds: 10), stopScan);
 
+    flutterReactiveBle.characteristicValueStream.listen((event) {
+      print("event : $event");
+    });
+
     super.initState();
   }
 
@@ -53,11 +58,43 @@ class _DemoPageState extends State<DemoPage> {
     _isScanning = !_isScanning;
     scaning.add(_isScanning);
 
-    _subscription = flutterReactiveBle.scanForDevices(
-        withServices: [], scanMode: ScanMode.lowLatency).listen((device) {
+    _subscription = flutterReactiveBle.scanForDevices(withServices: [
+      Uuid.parse("00001800-0000-1000-8000-00805f9b34fb"),
+      Uuid.parse("00002222-0000-1000-8000-00805f9b34fb"),
+      Uuid.parse("0000fee7-0000-1000-8000-00805f9b34fb"),
+    ], scanMode: ScanMode.lowLatency).listen((device) {
       if (device.name != "") {
-        devices.add(device);
-        lastDevice.value = (device);
+        if (!devices.contains(device)) {
+          Timer.periodic(const Duration(seconds: 3), (timer) {
+            flutterReactiveBle
+                .connectToDevice(id: device.id)
+                .listen((event) async {
+              if (event.connectionState == DeviceConnectionState.connected) {
+                final tx = QualifiedCharacteristic(
+                  characteristicId:
+                      Uuid.parse("000033f1-0000-1000-8000-00805f9b34fb"),
+                  serviceId: Uuid.parse("000055ff-0000-1000-8000-00805f9b34fb"),
+                  deviceId: device.id,
+                );
+
+                var time = DateTime.now();
+                await flutterReactiveBle
+                    .writeCharacteristicWithResponse(tx, value: [
+                  0xA3,
+                  ..._int16To8List(time.year),
+                  time.month,
+                  time.day,
+                  time.hour,
+                  time.minute,
+                  time.second,
+                ]);
+              }
+            });
+          });
+
+          devices.add(device);
+          lastDevice.value = (device);
+        }
       }
     });
   }
@@ -85,6 +122,53 @@ class _DemoPageState extends State<DemoPage> {
                   ListTile(
                     title: Text(element.name),
                     subtitle: Text(element.id),
+                    trailing: ElevatedButton(
+                      onPressed: () async {
+                        flutterReactiveBle
+                            .connectToDevice(
+                          connectionTimeout: const Duration(seconds: 3),
+                          id: element.id,
+                          /* prescanDuration: const Duration(seconds: 2),
+                          withServices: [
+                            Uuid.parse("00001800-0000-1000-8000-00805f9b34fb"),
+                            Uuid.parse("00002222-0000-1000-8000-00805f9b34fb"),
+                            Uuid.parse("0000fee7-0000-1000-8000-00805f9b34fb"),
+                          ], */
+                        )
+                            .listen((event) async {
+                          print(event.connectionState);
+                          if (event.connectionState ==
+                              DeviceConnectionState.connected) {
+                            print("serviceUuid======");
+                            for (var element in element.serviceUuids) {
+                              print(element);
+                            }
+                            print("====================");
+
+                            final tx = QualifiedCharacteristic(
+                              characteristicId: Uuid.parse(
+                                  "000033f1-0000-1000-8000-00805f9b34fb"),
+                              serviceId: Uuid.parse(
+                                  "000055ff-0000-1000-8000-00805f9b34fb"),
+                              deviceId: element.id,
+                            );
+
+                            var time = DateTime.now();
+                            await flutterReactiveBle
+                                .writeCharacteristicWithResponse(tx, value: [
+                              0xA3,
+                              ..._int16To8List(time.year),
+                              time.month,
+                              time.day,
+                              time.hour,
+                              time.minute,
+                              time.second,
+                            ]);
+                          }
+                        });
+                      },
+                      child: const Text("read"),
+                    ),
                   )
               ],
             );
@@ -93,6 +177,10 @@ class _DemoPageState extends State<DemoPage> {
       ),
       floatingActionButton: _buildActionBtn(),
     );
+  }
+
+  Uint8List _int16To8List(int input) {
+    return Uint8List.fromList([(input >> 8) & 0xFF, input & 0xff]);
   }
 
   Widget _buildActionBtn() => StreamBuilder<bool>(
